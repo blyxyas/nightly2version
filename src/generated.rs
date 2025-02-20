5a8a694ecfa37d95d10cc2a982927412c39f3676
blyxyas <blyxyas@gmail.com>
2025-02-20 19:41:23 +0100

    Sync to 1.85.0

--- src/generated.rs
+++ src/generated.rs
@@ -118,6 +118,7 @@ pub(crate) fn correlations_dates(minor: u16, patch: u16) -> Result<i64, &'static
         1 => Ok(1435253745),
         12 => Ok(1412872439),
         11 => Ok(1404324205),
+        85 => Ok(1740071365),
         84 if patch == 1 => Ok(1738262759),
         84 => Ok(1736437309),
         83 => Ok(1732802513),
@@ -249,6 +250,7 @@ pub(crate) fn correlations_commits(
         11 => Ok("f63a9c0cde3e69f0e283dd7dcd710a79a6869dee"),
         10 => Ok("d5678055c2bca22696e870f9103dc10aa6c35f41"),
         1 => Ok("bc3c16f09287e5545c1d3f76b7abd54f2eca868b"),
+        85 => Ok("ebfd33271586f0dd1bc6fe1016a0c38aae31f88b"),
         84 if patch == 1 => Ok("39e90365d9562dabea5c415e23838581e5d88ab1"),
         84 => Ok("a7abb900ebbe7f481d2a8826fbd0718b79ca58f5"),
         83 => Ok("5056432f7fbd9d936f350c0d0f990ca5df1d5e20"),
@@ -375,6 +377,7 @@ pub(crate) fn version_exists(minor: u16, patch: u16) -> bool {
         1 if patch == 0 => true,
         12 if patch == 0 => true,
         11 if patch == 0 => true,
+        85 if patch == 0 => true,
         84 if patch == 1 => true,
         84 if patch == 0 => true,
         83 if patch == 0 => true,
@@ -383,8 +386,9 @@ pub(crate) fn version_exists(minor: u16, patch: u16) -> bool {
     }
 }
 #[inline]
-pub(crate) fn all_versions() -> [((u16, u16, u16), i64); 120] {
+pub(crate) fn all_versions() -> [((u16, u16, u16), i64); 121] {
     [
+        ((1, 85, 0), 1740071365),
         ((1, 84, 1), 1738262759),
         ((1, 84, 0), 1736437309),
         ((1, 83, 0), 1732802513),
@@ -510,6 +514,7 @@ pub(crate) fn all_versions() -> [((u16, u16, u16), i64); 120] {
 #[inline]
 pub(crate) fn timestamp_ranges(timestamp: i64) -> Result<(u16, u16, u16), &'static str> {
     match timestamp - 1 {
+        1738262759..1740071365 => Ok((1, 85, 0)),
         1736437309..1738262759 => Ok((1, 84, 1)),
         1732802513..1736437309 => Ok((1, 84, 0)),
         1729183468..1732802513 => Ok((1, 83, 0)),
